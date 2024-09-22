4c246e9bd721adeb423512ac1b7877400ca1640e
wlads1963 <seredavn@mail.ru>
2024-09-22 14:37:06 +0300

    22/09

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+quests = 12
+watch = 1.5
+source = "Курс:"
+training_school = "Python,"
+time_for_task = watch / quests
+print(source,training_school,'всего задач:',quests,',','затрачено часов:',watch,', ''среднее время выполнения',time_for_task,'.')
